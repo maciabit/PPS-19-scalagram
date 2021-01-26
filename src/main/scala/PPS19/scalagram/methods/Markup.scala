6ac253d6aeb4a94c2c2b553a2d30233fe8e17091
Mattia <mattia.rossi15@studio.unibo.it>
2021-01-26 09:42:12 +0100

    Created Markup trait

--- src/main/scala/PPS19/scalagram/methods/Markup.scala
+++ src/main/scala/PPS19/scalagram/methods/Markup.scala
@@ -1,5 +1,12 @@
 package PPS19.scalagram.methods
 
 sealed trait Markup {
-
+  def selective: Boolean
 }
+
+object Markup
+
+case class KeyboardHide (selective: Boolean)
+case class KeyboardForceReply (selective: Boolean, single_use: Boolean)
+case class KeyboardMarkup (selective: Boolean, single_use: Boolean, resize: Boolean, rows: Vector[String])
+//case class InlineMarkup
--- src/main/scala/PPS19/scalagram/methods/Peer.scala
+++ src/main/scala/PPS19/scalagram/methods/Peer.scala
@@ -3,5 +3,6 @@ package PPS19.scalagram.methods
 sealed trait Peer {
   def chatId: String
 }
+
 object Peer
 case class InputPeerChat (chatId: String) extends Peer
