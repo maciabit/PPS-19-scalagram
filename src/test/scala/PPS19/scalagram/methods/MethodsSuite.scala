0429bf6bf5e13916ce3a8b92cf6ae87b34677d5f
Francesco Boschi <francesco.boschi2@studio.unibo.it>
2021-01-28 10:51:07 +0100

    Implemente deleteMessage and tested

--- src/test/scala/PPS19/scalagram/methods/MethodsSuite.scala
+++ src/test/scala/PPS19/scalagram/methods/MethodsSuite.scala
@@ -1,13 +1,17 @@
 package PPS19.scalagram.methods
 
 import PPS19.scalagram.utils.Props
+import org.scalatest.BeforeAndAfter
 import org.junit.runner.RunWith
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatestplus.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
-class MethodsSuite extends AnyFunSuite {
+class MethodsSuite extends AnyFunSuite with BeforeAndAfter {
 
+  before {
+    Props.load()
+  }
   /*test("Send a message") {
     Props.load()
     val p = InputPeerChat(chatId = "-1001364807173")
@@ -18,9 +22,12 @@ class MethodsSuite extends AnyFunSuite {
   }*/
 
   test("A Telegram API call can be performed using TelegramMethod Trait") {
-    Props.load()
     val text = GetNewUpdates().getNewUpdates()
     println(text)
     assert(true)
+  }
+
+  test("A Telegram message can be deleted with HTTP call"){
+    assert(DeleteMessage().deleteMessage(chatId = Left("-1001286594106"), messageId = 344))
   }
 }
