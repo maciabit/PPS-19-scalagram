67be39e8813d8e6f648afb8acee0ca99db719ccc
Mattia <mattia.rossi15@studio.unibo.it>
2021-01-29 10:02:22 +0100

    Refactor GetNewUpdates

--- src/main/scala/PPS19/scalagram/methods/GetNewUpdates.scala
+++ src/main/scala/PPS19/scalagram/methods/GetNewUpdates.scala
@@ -1,15 +1,18 @@
 package PPS19.scalagram.methods
 import PPS19.scalagram.models.Update
+import scala.util.{Try,Success,Failure}
 import io.circe.{HCursor, Json}
 import requests.Response
 import io.circe.parser._
+
+import scala.util.Try
 
 case class GetNewUpdates() {
   val method: Map[String, Any] => Response = TelegramMethod.method(HttpMethod.GET, "getUpdates")
   def getNewUpdates(offset: Option[Int] = None,
                     limit: Option[Int] = None,
                     timeout: Option[Int] = None,
-                    allowedUpdates: Option[Array[String]] = None): List[Update] = {
+                    allowedUpdates: Option[Array[String]] = None): Try[List[Update]] = {
     val urlParams: Map[String, Any] = Map(
       "offset" -> offset,
       "limit" -> limit,
@@ -17,13 +20,16 @@ case class GetNewUpdates() {
       "allowed_Updates" -> allowedUpdates,
     ) filter (_._2.isDefined) map { case (key, value) => (key, value.get) }
     val res = method(urlParams)
-    if (res.statusCode != 200) {
-      Left(parse(res.text()).getOrElse(Json.Null))
+    val parsed = parse(res.text()).getOrElse(Json.Null)
+    val updates = decode[List[Update]](parsed.findAllByKey("ok").head.toString())
+    updates match {
+      case Left(error) => Failure(error)
+      case Right(message) =>
+        val json = parsed.findAllByKey("result").head.asArray.get
+          .map(update => decode[Update](update.toString()))
+          .map { case Right(update) => update }
+          .toList
+        Success(json)
     }
-    val json = parse(res.text()).getOrElse(Json.Null).findAllByKey("result").head.asArray.get
-      .map(update => decode[Update](update.toString()))
-      .map { case Right(update) => update }
-      .toList
-    json
   }
 }
