ca88f0bf4caee6aff69838269199a00dfb36c4cd
Mattia <mattia.rossi15@studio.unibo.it>
2021-02-12 11:18:38 +0100

    InlineKeyboard test

--- src/main/scala/PPS19/scalagram/dsl/keyboard/InlineKeyboard.scala
+++ src/main/scala/PPS19/scalagram/dsl/keyboard/InlineKeyboard.scala
@@ -1,6 +1,11 @@
 package PPS19.scalagram.dsl.keyboard
 
-import PPS19.scalagram.models.{InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton}
+import PPS19.scalagram.dsl.keyboard.StringUtils.StringList
+import PPS19.scalagram.logic.{Bot, BotToken}
+import PPS19.scalagram.marshalling.codecs.EncoderOps
+import PPS19.scalagram.models.{ChatId, InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyMarkup}
+import PPS19.scalagram.utils.Props
+import io.circe.Encoder
 
 object InlineKeyboard {
 
@@ -14,4 +19,20 @@ object InlineKeyboard {
       case Left(button) => List(conversion(button))
       case Right(tuple) => tuple.map(conversion)
     }
+}
+
+object TryInlineKeyboard extends App {
+  import PPS19.scalagram.dsl.keyboard.Conversion._
+
+  val keyboard = InlineKeyboard(
+    "Button 1",
+    "Button 2" :: "Button 3"
+  )
+
+  println(Encoder[ReplyMarkup].snakeCase(keyboard))
+
+  val bot = Bot(BotToken(Props.get("token")))
+  val chatId = ChatId("-1001286594106")
+  bot.sendMessage(chatId, "Test InlineKeyboard DSL", replyMarkup = Some(keyboard))
+
 }
