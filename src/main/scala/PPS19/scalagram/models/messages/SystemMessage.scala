878dbd9bd3089958ff444357761c595cb8b466d7
Francesco Boschi <francesco.boschi2@studio.unibo.it>
2021-02-12 09:38:46 +0100

    Create model and teest for NewMembersAddedMessage and ChatMemberRemovedMessage

--- src/main/scala/PPS19/scalagram/models/messages/SystemMessage.scala
+++ src/main/scala/PPS19/scalagram/models/messages/SystemMessage.scala
@@ -1,6 +1,6 @@
 package PPS19.scalagram.models.messages
 
-import PPS19.scalagram.models.Chat
+import PPS19.scalagram.models.{Chat, User}
 import cats.implicits.toFunctorOps
 import io.circe.Decoder
 import io.circe.generic.semiauto.deriveDecoder
@@ -10,7 +10,9 @@ trait SystemMessage extends TelegramMessage {}
 object SystemMessage {
   implicit val systemMessageDecoder: Decoder[SystemMessage] =
     List[Decoder[SystemMessage]](
-      deriveDecoder[MessagePinned].widen
+      deriveDecoder[MessagePinned].widen,
+      deriveDecoder[ChatMembersAdded].widen,
+      deriveDecoder[ChatMemberRemoved].widen
     ).reduceLeft(_.or(_))
 }
 
@@ -19,4 +21,18 @@ final case class MessagePinned(
     chat: Chat,
     date: Int,
     pinnedMessage: TelegramMessage
+) extends SystemMessage
+
+final case class ChatMembersAdded(
+    messageId: Int,
+    chat: Chat,
+    date: Int,
+    newChatMembers: Seq[User]
+) extends SystemMessage
+
+final case class ChatMemberRemoved(
+    messageId: Int,
+    chat: Chat,
+    date: Int,
+    leftChatMember: User
 ) extends SystemMessage
