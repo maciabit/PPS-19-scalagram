5a9e17021311d3f4bb3dfa1e4d4fb851e872be6d
Mattia <mattia.rossi15@studio.unibo.it>
2021-02-16 12:50:58 +0100

    Created KeyboardButton and ReplyMarkup traits

--- src/main/scala/PPS19/scalagram/models/ReplyMarkup.scala
+++ src/main/scala/PPS19/scalagram/models/ReplyMarkup.scala
@@ -2,31 +2,41 @@ package PPS19.scalagram.models
 
 import io.circe.Encoder
 import io.circe.generic.auto._
+import io.circe.generic.semiauto.deriveEncoder
 import io.circe.syntax._
 
 sealed trait ReplyMarkup
 
 object ReplyMarkup {
   implicit val encodeReplyMarkup: Encoder[ReplyMarkup] = Encoder.instance {
-    case inlineKeyboardMarkup: InlineKeyboardMarkup =>
-      inlineKeyboardMarkup.asJson.deepDropNullValues
-    case replyKeyboardMarkup: ReplyKeyboardMarkup =>
-      replyKeyboardMarkup.asJson.deepDropNullValues
+    case keyboardMarkup: KeyboardMarkup =>
+      KeyboardMarkup.encodeKeyboardMarkup(keyboardMarkup)
     case replyKeyboardRemove: ReplyKeyboardRemove =>
       replyKeyboardRemove.asJson.deepDropNullValues
     case forceReply: ForceReply => forceReply.asJson.deepDropNullValues
   }
 }
 
+trait KeyboardMarkup extends ReplyMarkup
+
+object KeyboardMarkup {
+  implicit val encodeKeyboardMarkup: Encoder[KeyboardMarkup] = Encoder.instance {
+    case inlineKeyboardMarkup: InlineKeyboardMarkup =>
+      inlineKeyboardMarkup.asJson.deepDropNullValues
+    case replyKeyboardMarkup: ReplyKeyboardMarkup =>
+      replyKeyboardMarkup.asJson.deepDropNullValues
+  }
+}
+
 case class InlineKeyboardMarkup(inlineKeyboard: Seq[Seq[InlineKeyboardButton]])
-    extends ReplyMarkup
+    extends KeyboardMarkup
 
 case class ReplyKeyboardMarkup(
-    keyboard: Seq[Seq[KeyboardButton]],
+    keyboard: Seq[Seq[ReplyKeyboardButton]],
     resizeKeyboard: Option[Boolean] = None,
     oneTimeKeyboard: Option[Boolean] = None,
     selective: Option[Boolean] = None
-) extends ReplyMarkup
+) extends KeyboardMarkup
 
 case class ReplyKeyboardRemove(
     removeKeyboard: Boolean = true,
